bbfb8875d6f9317e6c5d0ccd6dfebd9f8cd68e60
toviazs <tobias.pazposse@alu.frt.utn.edu.ar>
2023-11-02 14:37:59 -0300

    Agregar ResultDTO

---
+++ APIv2/Models/DTO/ResultDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace APIv2.Models.DTO
+{
+    public class ResultDTO<T>
+    {
+        public T Results { get; set; }
+        public List<string> ErrorsMessages { get; set; }
+        public List<string> Messages { get; set; }
+        public int StatusCode { get; set; }
+
+        public ResultDTO()
+        {
+            ErrorsMessages = new List<string>();
+            Messages = new List<string>();
+        }
+    }
+}
