09028acf135599ee13599368bb363228485f667f
toviazs <tobias.pazposse@alu.frt.utn.edu.ar>
2023-11-03 00:50:46 -0300

    Agregar e implementar IHistorialrolRepository e HistorialRolRepository

---
+++ Repositories/Contracts/IHistorialrolRepository.cs
@@ -0,0 +1,15 @@
+﻿using APIv2.Data;
+using APIv2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Contracts
+{
+    public interface IHistorialrolRepository : IBaseRepository
+    {
+        public List<Historialrol> GetManyByLegajo(int legajoEmp);
+    }
+}
---
+++ Repositories/HistorialrolRepository.cs
@@ -0,0 +1,26 @@
+﻿using APIv2.Data;
+using APIv2.Models;
+using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class HistorialrolRepository : BaseRepository, IHistorialrolRepository
+    {
+        private PersonalDB _personalDb;
+
+        public HistorialrolRepository(PersonalDB personalDb) : base(personalDb)
+        {
+            _personalDb = personalDb;
+        }
+
+        public List<Historialrol> GetManyByLegajo(int legajoEmp)
+        {
+            return _personalDb.Historialrols.Where(histRol => histRol.LegajoEmpleado == legajoEmp).ToList();
+        }
+    }
+}
