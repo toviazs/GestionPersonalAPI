b302f2c7450dcc5eba84e33f28e6866e073e7527
toviazs <tobias.pazposse@alu.frt.utn.edu.ar>
2023-11-03 00:51:27 -0300

    Agregar e implementar IHistorialrolService e HistorialrolService

---
+++ Services/Contracts/IHistorialrolService.cs
@@ -0,0 +1,14 @@
+﻿using APIv2.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracts
+{
+    public interface IHistorialrolService
+    {
+        public List<HistorialrolDTO> GetManyByLegajo(int legajoEmp);
+    }
+}
---
+++ Services/HistorialrolService.cs
@@ -0,0 +1,32 @@
+﻿using APIv2.Models.DTO;
+using Mappers;
+using Mappers.Contracts;
+using Repositories;
+using Repositories.Contracts;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class HistorialrolService : IHistorialrolService
+    {
+        private readonly IHistorialrolRepository _histRolRepository;
+        private readonly IHistorialrolMapper _histRolMapper;
+        public HistorialrolService(IHistorialrolRepository histRolRepository, IHistorialrolMapper histRolMapper)
+        {
+            _histRolRepository = histRolRepository;
+            _histRolMapper = histRolMapper;
+        }
+        public List<HistorialrolDTO> GetManyByLegajo(int legajoEmp)
+        {
+            List<HistorialrolDTO> histRoles = new List<HistorialrolDTO>();
+            histRoles = _histRolRepository.GetManyByLegajo(legajoEmp)
+                .Select(histRol => _histRolMapper.MapToHistorialrolDTO(histRol)).ToList();
+            return histRoles.OrderBy(histRol => histRol.FechaCambio).ToList();
+        }
+    }
+}
