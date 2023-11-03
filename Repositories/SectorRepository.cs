475357173355102080a186e2558be4a2723ebfe4
toviazs <tobias.pazposse@alu.frt.utn.edu.ar>
2023-11-03 00:05:08 -0300

    Agregar e implementar ISectorRepository y SectorRepository

---
+++ Repositories/SectorRepository.cs
@@ -0,0 +1,29 @@
+﻿using APIv2.Data;
+using APIv2.Models;
+using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class SectorRepository : BaseRepository, ISectorRepository
+    {
+        private PersonalDB _personalDb;
+        public SectorRepository(PersonalDB personalDb) : base(personalDb)
+        {
+            _personalDb = personalDb;
+        }
+        public List<Sector> GetAll()
+        {
+            return _personalDb.Sectors.ToList();
+        }
+
+        public Sector? GetById(int idSector)
+        {
+            return _personalDb.Sectors.FirstOrDefault(sector => sector.IdSector == idSector);
+        }
+    }
+}
