27aa1ca61855da05931bc16fe772ffafd346e017
toviazs <tobias.pazposse@alu.frt.utn.edu.ar>
2023-11-03 00:13:00 -0300

    Corregir interfaz de repo sector ISectorRepository

--- Repositories/Contracts/ISectorRepository.cs
+++ Repositories/Contracts/ISectorRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using APIv2.Data;
+using APIv2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,9 @@ using System.Threading.Tasks;
 
 namespace Repositories.Contracts
 {
-    internal interface ISectorRepository
+    public interface ISectorRepository : IBaseRepository
     {
+        public List<Sector> GetAll();
+        public Sector? GetById(int idSector);
     }
 }
