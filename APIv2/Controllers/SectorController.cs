9dd28cef46a3bedd0ad9f6825d35d91b4f477dc0
toviazs <tobias.pazposse@alu.frt.utn.edu.ar>
2023-11-03 00:20:03 -0300

    Implementar SectorController con toda su funcionalidad

---
+++ APIv2/Controllers/SectorController.cs
@@ -0,0 +1,60 @@
+﻿using APIv2.Mappers.Contracts;
+using APIv2.Models;
+using APIv2.Data;
+using APIv2.Models.DTO;
+using APIv2.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using APIv2.Data.Configurations;
+using System.ComponentModel.DataAnnotations;
+using Validators.Contracts;
+using Mappers.Contracts;
+using Microsoft.AspNetCore.Identity;
+using APIv2.Services;
+using Services.Contracts;
+
+namespace APIv2.Controllers
+{
+    [ApiController]
+    [Route("Sector")]
+    public class SectorController : Controller
+    {
+        private readonly ISectorService _sectorService;
+        private readonly ISectorMapper _sectorMapper;
+
+        public SectorController(ISectorService sectorService, ISectorMapper sectorMapper)
+        {
+            _sectorService = sectorService;
+            _sectorMapper = sectorMapper;
+        }
+
+        [HttpGet]
+        public ActionResult GetSectores()
+        {
+            ResultDTO<List<SectorDTO>> result = new ResultDTO<List<SectorDTO>>();
+            result.Results = _sectorService.GetAll();
+            result.StatusCode = Ok().StatusCode;
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("{idSector:int}")]
+        public ActionResult GetSectorById([FromRoute] int idSector)
+        {
+            ResultDTO<SectorDTO> result = new ResultDTO<SectorDTO>();
+            SectorDTO? sector = _sectorService.GetById(idSector);
+            if (sector == null)
+            {
+                result.ErrorsMessages.Add("Sector no encontrado!");
+                result.StatusCode = NotFound().StatusCode;
+                return NotFound(result);
+            }
+            else
+            {
+                result.Results = sector;
+                result.StatusCode = Ok().StatusCode;
+                return Ok(result);
+            }
+        }
+
+    }
+}
