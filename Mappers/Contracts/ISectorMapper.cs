db5a97ed6d9d4403e8272bb1b8f489e223963da8
toviazs <tobias.pazposse@alu.frt.utn.edu.ar>
2023-11-02 23:59:51 -0300

    Agregar e implementar ISectorMapper y SectorMapper

---
+++ Mappers/Contracts/ISectorMapper.cs
@@ -0,0 +1,16 @@
+﻿using APIv2.Models.DTO;
+using APIv2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mappers.Contracts
+{
+    public interface ISectorMapper
+    {
+        public Sector MapToSector(SectorDTO sectorDTO);
+        public SectorDTO MapToSectorDTO(Sector sector);
+    }
+}
---
+++ Mappers/SectorMapper.cs
@@ -0,0 +1,40 @@
+﻿using APIv2.Models;
+using APIv2.Models.DTO;
+using Mappers.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mappers
+{
+    public class SectorMapper : ISectorMapper
+    {
+        public Sector MapToSector(SectorDTO sectorDTO)
+        {
+            Sector sector = new Sector
+            {
+                IdSector = sectorDTO.IdSector,
+                NombreSector = sectorDTO.NombreSector,
+                DescripcionSector = sectorDTO.DescripcionSector,
+                EstadoSector = sectorDTO.EstadoSector
+            };
+
+            return sector;
+        }
+
+        public SectorDTO MapToSectorDTO(Sector sector)
+        {
+            SectorDTO sectorDTO = new SectorDTO
+            {
+                IdSector = sector.IdSector,
+                NombreSector = sector.NombreSector,
+                DescripcionSector = sector.DescripcionSector,
+                EstadoSector = sector.EstadoSector
+            };
+
+            return sectorDTO;
+        }
+    }
+}
